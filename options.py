f60e17aac9ac46aeb720eb33d97c82277e59bad0
chdwyb <wyb@chd.edu.cn>
2023-08-01 23:08:36 +0800

    second commit

--- options.py
+++ options.py
@@ -13,15 +13,15 @@ class Options():
         self.Input_Path_Train = 'I://remote_sensing_haze_dataset/Haze1k_thick/train/input/'
         self.Target_Path_Train = 'I://remote_sensing_haze_dataset/Haze1k_thick/train/target/'
         # validation
-        self.Input_Path_Val = 'I://remote_sensing_haze_dataset/Haze1k_thick/test/input/'
-        self.Target_Path_Val = 'I://remote_sensing_haze_dataset/Haze1k_thick/test/target/'
+        self.Input_Path_Val = 'I://remote_sensing_haze_dataset/Haze1k_thick/val/input/'
+        self.Target_Path_Val = 'I://remote_sensing_haze_dataset/Haze1k_thick/val/target/'
         # testing
         self.Input_Path_Test = 'I://remote_sensing_haze_dataset/Haze1k_thick/test/input/'
         self.Target_Path_Test = 'I://remote_sensing_haze_dataset/Haze1k_thick/test/target/'
         self.Result_Path_Test = 'I://remote_sensing_haze_dataset/Haze1k_thick/test/result/'
 
         self.MODEL_SAVE_PATH = './'
-        self.MODEL_PRE_PATH = './model_best.pth'
+        self.MODEL_PRE_PATH = './rs_haze.pth'
 
         self.Num_Works = 4
         self.CUDA_USE = True
